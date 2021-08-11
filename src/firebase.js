a5f3614e5e60db2927f5f93cb4567af027d7bec1
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-11 11:13:32 +0300

    Edit name error

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseApp = firebase.initiAlizeApp({
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyD74USIzlkdclDTl3jHkWwoApOy4DP_hoU",
     authDomain: "my-basic-chat-app-cc633.firebaseapp.com",
     projectId: "my-basic-chat-app-cc633",
