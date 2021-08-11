54d445e50240c6ee9742b4beeb2a19557cf1c63c
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-11 11:13:56 +0300

    Add condition to SignIn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import SignIn from './components/SignIn';
+import Chat from './components/Chat';
+import {auth} from './firebase.js'
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <>
-
+    {user ? <Chat /> : <SignIn />}
+      
     </>
   );
 }
