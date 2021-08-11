ae48f9acb25ad6aad69240c4bf36076385ff64f6
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-11 13:01:51 +0300

    Import messages collection from db

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase';
+import SendMessage from './SendMessage';
 import SignOut from './SignOut'
 
 function Chat() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
   return (
     <div>
       Chat
       <SignOut />
+      {messages.map(({id, text, photoURL}) => (
+        <div key={id}>
+          <img src={photoURL} alt="" /> 
+          <p>{text}</p> 
+        </div>
+      ))}
+      <SendMessage />
     </div>
   )
 }
