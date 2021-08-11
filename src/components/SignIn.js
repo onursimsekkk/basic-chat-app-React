8f13ccda418c35664d2db36639f3c54dd70c1b15
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-11 11:11:49 +0300

    Add SignIn component

---
+++ src/components/SignIn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import firebase from 'firebase'
+import {auth} from '../firebase.js'
+import {Button} from '@material-ui/core'
+
+function SignIn() {
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <div>
+      <Button onClick={signInWithGoogle} >Sign in with Google</Button>
+    </div>
+  )
+}
+
+export default SignIn
