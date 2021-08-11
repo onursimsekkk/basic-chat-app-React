3b008ae754710f7558a38c925bfffb3adda6197a
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-11 14:52:55 +0300

    Create SendMessage comp

---
+++ src/components/SendMessage.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import firebase from 'firebase';
+import { db, auth } from '../firebase';
+import { Input, Button} from '@material-ui/core';
+
+function SendMessage({ scroll }) {
+  const [msg, setMsg] = useState("");
+
+  async function sendMessage(e){
+    e.preventDefault();
+    const {uid, photoURL} = auth.currentUser;
+
+    await db.collection('messages').add({
+      text: msg,
+      photoURL,
+      uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setMsg("")
+    scroll.current.scrollIntoView({ behaviour: 'smooth' })
+  }
+  return (
+    <div>
+      <form onSubmit={sendMessage}>
+        <Input value={msg} onChange={(e) => setMsg(e.target.value)} placeholder="Message..." />
+        <Button type="submit">Send</Button>
+      </form>
+    </div>
+  )
+}
+
+export default SendMessage
