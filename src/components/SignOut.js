7af5f82fe71951b4437dd8863ec67d6448e45263
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-12 11:35:41 +0300

    Add inline styles

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -4,8 +4,10 @@ import {Button} from '@material-ui/core'
 
 function SignOut() {
   return (
-    <div>
-      <Button onClick={() => auth.signOut()} >Sign Out</Button>
+    <div style={{
+      display: 'flex', justifyContent: 'center', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, borderBottom: 'solid 1px lightgray', zIndex: '10'
+  }}>
+      <Button style={{ padding: '20px', fontSize: '15px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()} >Sign Out</Button>
     </div>
   )
 }
